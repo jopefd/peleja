03848332c7b4fff3b3919b5f50bc5ba363eaae9b
João Pedro F. Dantas <joaopedrofdantas@tutanota.com>
2021-10-04 21:45:21 -0300

    URI-1743

--- uri/1743/src.cpp
+++ uri/1743/src.cpp
@@ -1,9 +1,13 @@
+///
+/// \author João Pedro F. Dantas
+/// \date 2021-10-04
 /// \see https://www.urionlinejudge.com.br/judge/en/problems/view/1743
+///
 
 #include <bits/stdc++.h>
 
 using namespace std;
- 
+
 int main() {
     string conector1, conector2;
     bool combinam{true};
@@ -12,15 +16,13 @@ int main() {
     getline(cin, conector2);
 
     for (int i{0}; i < conector1.length(); i += 2) {
-        if (conector1[i] == conector2[i])
-            combinam = false;
+        if (conector1[i] == conector2[i]) combinam = false;
     }
 
     if (combinam)
         cout << "Y\n";
     else
         cout << "N\n";
- 
+
     return 0;
 }
-
